12c5ed777356a0eaa95197137581cc3baf67e317
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-26 11:47:19 +0200

    Add basic styling

--- app/controllers/select-road-sign.js
+++ app/controllers/select-road-sign.js
@@ -1,13 +1,16 @@
 import Controller from '@ember/controller';
+import { and } from '@ember/object/computed';
 
 export default Controller.extend({
   urlGnApp: 'http://dev.gelinkt-notuleren.lblod.info',
-  mockLogin: false,
+  mockLogin: true,
+
+  importEnabled: and('urlGnApp', 'containerUri'),
 
   actions: {
-    async sendRoadSign() {
+    importInEditor() {
       if (this.urlGnApp && this.containerUri) {
-        window.location.replace(this.urlGnApp+'/fetch-rdfa?source=<https://dev.kleinbord.lblod.info/input/example.html>&target=<'+this.containerUri+'>&mock='+this.mockLogin);
+        window.location.replace(`${this.urlGnApp}/fetch-rdfa?source=https://dev.kleinbord.lblod.info/input/example.html&target=${this.containerUri}&mock=${this.mockLogin}`);
       }
     }
   }
