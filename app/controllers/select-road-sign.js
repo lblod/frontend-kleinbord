2b06ea02d00ab26526845a604648e9541fa342e3
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-08-26 12:46:31 +0200

    small updates to handle spec changes

--- app/controllers/select-road-sign.js
+++ app/controllers/select-road-sign.js
@@ -1,13 +1,22 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  urlGnApp: 'http://dev.gelinkt-notuleren.lblod.info',
+  urlGnApp: 'http://dev.gelinkt-notuleren.lblod.info/import/edit',
+  sourceUrl: 'https://dev.kleinbord.lblod.info/snippets/example.html',
   mockLogin: false,
 
   actions: {
     async sendRoadSign() {
-      if (this.urlGnApp && this.containerUri) {
-        window.location.replace(this.urlGnApp+'/fetch-rdfa?source=<https://dev.kleinbord.lblod.info/input/example.html>&target=<'+this.containerUri+'>&mock='+this.mockLogin);
+      if (this.urlGnApp) {
+        let params = [];
+        params.push(`source=${this.sourceUrl}`);
+
+        if(this.containerUri)
+          params.push(`target=${this.containerUri}`);
+
+        params.push(`mock=${this.mockLogin}`);
+
+        window.location.replace(this.urlGnApp +`?${params.join('&')}`);
       }
     }
   }
